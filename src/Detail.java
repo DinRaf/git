af68c48fad7d825a0045632164c84bc9f140384e
Dinar <rafteri98@gmail.com>
2017-06-07 10:05:26 +0300

    add

---
+++ src/Detail.java
@@ -0,0 +1,66 @@
+
+public class Detail implements Comparable<String> {
+	private String material;
+	private String purpose;
+
+	private Integer weight;
+
+	private Integer price;
+	public Detail(String material, String purpose, Integer weight, Integer price) {
+		this.material = material;
+		this.purpose = purpose;
+		this.weight = weight;
+		this.price = price;
+	}
+
+	public String getMaterial() {
+		return material;
+	}
+
+	public Detail setMaterial(String material) {
+		this.material = material;
+		return this;
+	}
+
+	public String getPurpose() {
+		return purpose;
+	}
+
+	public Detail setPurpose(String purpose) {
+		this.purpose = purpose;
+		return this;
+	}
+
+	public Integer getWeight() {
+		return weight;
+	}
+
+	public Detail setWeight(Integer weight) {
+		this.weight = weight;
+		return this;
+	}
+
+	public Integer getPrice() {
+		return price;
+	}
+
+	public Detail setPrice(Integer price) {
+		this.price = price;
+		return this;
+	}
+
+	@Override
+	public int compareTo(String o) {
+		return this.purpose.compareTo(o);
+	}
+
+	@Override
+	public String toString() {
+		return "Detail{" +
+				"material='" + material + '\'' +
+				", purpose='" + purpose + '\'' +
+				", weight=" + weight +
+				", price=" + price +
+				'}';
+	}
+}
